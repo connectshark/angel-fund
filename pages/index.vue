61f355a4de11c0b620b13b534167d1578b356cc5
Chambers <bobhus394@gmail.com>
2024-10-09 18:04:11 +0800

    更新改用api撈文章

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 <NuxtLayout>
   <h1 class=" text-center text-3xl/loose">天使會文章大全</h1>
   <ul class=" mx-auto w-11/12 max-w-3xl space-y-4">
-    <li v-for="(item, i) in data">
-      <NuxtLink class="hover:underline text-lg/loose" :to="`/article/` + item.link">{{ i + 1 }}.{{ item.content }}</NuxtLink>
+    <li v-for="(item, i) in data.list">
+      <NuxtLink class="hover:underline text-lg/loose" :to="`/article/` + item.topicId">{{ i + 1 }}.{{ item.title }}</NuxtLink>
     </li>
   </ul>
 </NuxtLayout>
@@ -12,7 +12,7 @@
 <script setup>
 const {
   data
-} = await useFetch('/api/fund')
+} = await useFetch('/api/fund?page=1')
 
 useHead({
   title: ''
